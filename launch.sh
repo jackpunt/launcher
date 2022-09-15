34ab5dedb9100dea52b8161c19c8903acad7941a
jackpunt <jpeckj@gmail.com>
2022-09-14 22:36:31 -0700

    launch.sh $giid

--- launch.sh
+++ launch.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 # provoke launch of gi184.json
+giid=${1:=148}
 url="https://game5.thegraid.com:8445/api/launcher/launch"
-curl $url -d@gi184.json -H "Content-Type: application/json" -H "Authorization: Bearer $(cat ./tokn)" && echo
+curl $url -d@gi$giid.json -H "Content-Type: application/json" -H "Authorization: Bearer $(cat ./tokn)" && echo
