1999bb25ddae349c5d3e409c8bd6762f694807bc
jackpunt <jpeckj@gmail.com>
2022-10-28 17:00:31 -0700

    npm update & minor cleanup

--- src/main/java/com/thegraid/config/ApplicationProperties.java
+++ src/main/java/com/thegraid/config/ApplicationProperties.java
@@ -13,15 +13,4 @@ public class ApplicationProperties {
     // jhipster-needle-application-properties-property
     // jhipster-needle-application-properties-property-getter
     // jhipster-needle-application-properties-property-class
-
-    // When using env.getProperty() or @Value("${prop.path}") we don't need these:
-    // (which enable props.thegraid.assetBase)
-    // Thegraid thegraid = new Thegraid();
-    // public Thegraid getThegraid() { return thegraid; }
-
-    // public class Thegraid {
-    //     public String assetBase;
-    //     public String getAssetBase() { return assetBase; };
-    //     public void setAssetBase(String base) { assetBase = base; }
-    // }
 }
